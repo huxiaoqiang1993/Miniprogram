276d5786ccab96fc164119eb2f7687801aa4b761
huxiaoqiang1993 <1064301046@qq.com>
2021-01-11 11:17:22 +0800

    Miniprogram

---
+++ pages/index/index.js
@@ -0,0 +1,16 @@
+// index.js
+Page({
+  data:{
+    name:'HuXiaoqiang',
+    age:'27',
+    students:[
+      {id:201603670,name:'胡小强',age:27},
+      {id:201603671,name:'邱印',age:27},
+      {id:201603672,name:'康锐',age:26},
+      {id:201603673,name:'白吴',age:25},
+      {id:201603674,name:'王攀',age:25},
+      {id:201603675,name:'旷识礼',age:25},
+      {id:201603676,name:'扈豪',age:28},
+    ]
+  }
+})
